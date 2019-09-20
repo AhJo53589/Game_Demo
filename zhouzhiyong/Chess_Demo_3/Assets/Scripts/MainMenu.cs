325ad2ba3e32de9394560c89e7d03363b716321f
Ikaruga <ahjo_bb@hotmail.com>
2019-09-20 18:30:40 +0800

    update

---
+++ zhouzhiyong/Chess_Demo_3/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+    public Button m_Button_Play;
+    public Button m_Button_Quit;
+	// Use this for initialization
+	void Start ()
+    {
+        m_Button_Play.onClick.AddListener(OnButtonPlayClick);
+        m_Button_Quit.onClick.AddListener(OnButtonQuitClick);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void OnButtonPlayClick()
+    {
+        JumpScene("Game");
+    }
+
+    void OnButtonQuitClick()
+    {
+        Application.Quit();
+    }
+
+    void JumpScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+}
