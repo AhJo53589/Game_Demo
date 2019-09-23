13f6ff342d201a35c01e7e4f4d8ab794d68b9e45
Ikaruga <ahjo_bb@hotmail.com>
2019-09-23 18:32:24 +0800

    update

--- zhouzhiyong/Chess_Demo_3/Assets/Scripts/MainMenu.cs
+++ zhouzhiyong/Chess_Demo_3/Assets/Scripts/MainMenu.cs
@@ -9,22 +9,38 @@ public class MainMenu : MonoBehaviour
 {
     public Button m_Button_Play;
     public Button m_Button_Quit;
+    public Button[] m_Button_Level;
+    public Slider m_Slider_Anim;
+
+    
 	// Use this for initialization
 	void Start ()
     {
         m_Button_Play.onClick.AddListener(OnButtonPlayClick);
         m_Button_Quit.onClick.AddListener(OnButtonQuitClick);
+
+        foreach (Button btn in m_Button_Level)
+        {
+            btn.gameObject.SetActive(false);
+            btn.onClick.AddListener(() =>
+            {
+                //string str = btn.transform.Find("Text").GetComponent<Text>().text.ToString();
+                string str = btn.GetComponentInChildren<Text>().text.ToString();
+                //string str = "Level 01";
+                JumpScene(str);
+            });
+        }
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
-	}
+    }
 
     void OnButtonPlayClick()
     {
-        JumpScene("Game");
+        StartCoroutine(SliderRun());
+        Debug.Log("OnButtonPlayClick");
     }
 
     void OnButtonQuitClick()
@@ -32,8 +48,32 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
     }
 
+    void OnButtonLevel01Click()
+    {
+        JumpScene("Level 01");
+    }
+
     void JumpScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+
+
+    IEnumerator SliderRun()
+    {
+        while (true)
+        {
+            if (m_Slider_Anim.value < 10)
+            {
+                m_Slider_Anim.value += Time.deltaTime;
+            }
+            else
+            {
+                Debug.Log("SliderRun");
+                m_Button_Level[0].gameObject.SetActive(true);
+                yield break;
+            }
+            yield return 0;
+        }
     }
 }
